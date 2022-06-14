d5170dd281e292a580b032aa47bf5892be4ca5c8
m.mitsuwa <kumapyonpyon@yahoo.co.jp>
2022-06-15 06:42:22 +0900

    New

---
+++ VBComponents.ps1
@@ -0,0 +1,15 @@
+﻿param([string]$fileName)  
+  
+$filePath = Join-Path $pwd $fileName  
+  
+$excel = New-Object -ComObject Excel.Application  
+  
+$excel.Workbooks.Open($filePath) | % {  
+  $_.VBProject.VBComponents | % {  
+    $exportFileName = Join-Path $pwd ($_.Name + ".bas")  
+    $_.Export($exportFileName)  
+  }  
+}  
+  
+$excel.Quit()  
+
